d32d94a8ca83f7ed1e2378be2bdcec10907804a9
twesigetrejan <trejantwesige19@gmail.com>
2024-03-09 00:21:54 +0300

    my first commit

---
+++ users/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm 
+def home(request):
+    return HttpResponse('<h1>Hello there</h1>')
+
+def register(request):
+    if request.method =='POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account has been created for {username}!')
+            return redirect('site-home')
+            
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
