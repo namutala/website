624fb8d39a7ebdd145246e57750a61e2a35a1608
twesigetrejan <trejantwesige19@gmail.com>
2024-03-12 17:15:54 +0300

    completing the user profile

--- localbizconnect/urls.py
+++ localbizconnect/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as users_views
 
 
@@ -24,6 +26,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('register/',users_views.register, name="register"),
+    path('profile/', users_views.profile, name ='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
+
+if settings.DEBUG :
+    urlpatterns += static(settings.MEDIA_URL, document_root  = settings.MEDIA_ROOT)
+    
+    
--- users/models.py
+++ users/models.py
@@ -4,10 +4,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to = 'profille_pics')
-    Biography = models.TextField(max_length = 100)
     
 
-    
     def __str__(self):
         return f'{self.user.username} Profile'
     
