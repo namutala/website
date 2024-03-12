a24b1c612370023e614eeca2a071805310857114
twesigetrejan <trejantwesige19@gmail.com>
2024-03-12 15:02:38 +0300

    user profile

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to = 'profille_pics')
+    Bio = models.TextField(max_length = 100)
 
     
     def __str__(self):
