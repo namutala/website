81437d15f9715a12ae62790300ba400370fe64cc
Androbuzz <andricishami@gmail.com>
2024-03-13 23:59:36 +0300

    Modified settings

--- localbizconnect/settings.py
+++ localbizconnect/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'localbizconnect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
