14ea2e2a477cd639833efda60bfdb15457387a60
Androbuzz <andricishami@gmail.com>
2024-03-14 12:27:00 +0300

    Added relations

--- users/models.py
+++ users/models.py
@@ -34,6 +34,6 @@ class Menu(models.Model):
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=1)
     def __str__(self):
         return f'{self.name}'
