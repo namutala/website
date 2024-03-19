9a2de8cb6cce743542d666d46e0b3969e17b63af
Androbuzz <andricishami@gmail.com>
2024-03-19 16:29:23 +0300

    Added a business view

--- business/models.py
+++ business/models.py
@@ -3,16 +3,17 @@ from django.contrib.auth.models import User
 from PIL import Image
 
 class Business(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     business_name = models.CharField(max_length=25, default ='Business name')
     logo = models.ImageField(default='default.jpg', upload_to='business_logos')
     business_description = models.TextField(max_length=150)
     location = models.TextField()
     email = models.EmailField()
     contact = models.IntegerField()
+    photos = models.ManyToManyField('BusinessImage', related_name='businesses', blank=True)
 
     def _str_(self):
-        return f'{self.user.username} Business'
+        return f'{self.owner.username} Business'
 
 class BusinessImage(models.Model):
     business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='catalogue_images')
--- business/views.py
+++ business/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Business
 
 
+@login_required(login_url='/login')
+def register_business(request):
+    if request.method == 'POST':
+        return HttpResponse('Business Registered Successfully')
+    else:
+        return render(request, 'business/business.html')
