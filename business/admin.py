22dcd236764dcad3743a14d9769f4ab998d21732
twesigetrejan <trejantwesige19@gmail.com>
2024-03-19 13:17:07 +0300

    business registration with images

--- business/admin.py
+++ business/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Business, BusinessImage
 
-# Register your models here.
+admin.site.register(Business)
+admin.site.register(BusinessImage)
--- business/models.py
+++ business/models.py
@@ -4,15 +4,20 @@ from PIL import Image
 
 class Business(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    Business_name = models.TextField(max_length = 25)
-    logo = models.ImageField(default='default.jpg', upload_to = 'business_logos')
-    Business_description = models.TextField(max_length= 150)
+    business_name = models.CharField(max_length=25, default ='Business name')
+    logo = models.ImageField(default='default.jpg', upload_to='business_logos')
+    business_description = models.TextField(max_length=150)
     location = models.TextField()
-    email =models.EmailField()
-    Conctact = models.IntegerField()
-    Cartalogue = models.ImageField()
-    
-    def __str(self):
+    email = models.EmailField()
+    contact = models.IntegerField()
+
+    def _str_(self):
         return f'{self.user.username} Business'
-    
+
+class BusinessImage(models.Model):
+    business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='catalogue_images')
+    image = models.ImageField(upload_to='business_catalogue')
+
+    def _str_(self):
+        return f'Image for {self.business.business_name}'
 
