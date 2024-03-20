3f46f70acf546a84eaaf044bf3b2f09d4c60b52b
CodexEmmy <emmyimmanuelasiimwe@gmail.com>
2024-03-20 19:46:55 +0300

    storefront

--- storefront/models.py
+++ storefront/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200,null=True,blank=True)
+    phone_number = models.CharField(max_length=200,null=True,blank=True)
+    email = models.EmailField(max_length=200,null=True,blank=True)
+    address = models.CharField(max_length=200,null=True,blank=True)
+    city = models.CharField(max_length=200,null=True,blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return f'{self.name}'
+    
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+        )
+    name = models.CharField(max_length=200, null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True, choices=CATEGORY)
+    description = models.TextField(null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out of delivery', 'Out of delivery'),
+        ('Delivered', 'Delivered'),
+             )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200,null=True,blank=True)
+
