7cb18486aa8b84ba421284135d986090a70d4dd7
twesigetrejan <trejantwesige19@gmail.com>
2024-03-21 23:36:59 +0300

    storefront urls

--- localbizconnect/urls.py
+++ localbizconnect/urls.py
@@ -32,7 +32,8 @@ urlpatterns = [
     path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name="password_reset_done"),
     path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name="password_reset_complete"),
     path('', include('users.urls')),
-    path('business/', include('business.urls'))
+    path('business/', include('business.urls')),
+    path('storefront/', include('storefront.urls'))
 ]
 
 if settings.DEBUG :
--- storefront/urls.py
+++ storefront/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('',views.Product_details, name ="product-info"),
+    path('order_details/', views.Order_details, name= "order-info")
+    
+]
+
