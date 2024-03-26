db040274ad6d6d6036e839598c1eb9afc2db4aca
twesigetrejan <trejantwesige19@gmail.com>
2024-03-27 01:01:03 +0300

    models

--- bookings/models.py
+++ bookings/models.py
@@ -1,12 +1,18 @@
 from django.db import models
-from business.models import Business
-from users.models import User
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-class Booking(models.Model):
-    business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='bookings')
-    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
-    date = models.DateField()
-    time = models.TimeField()
+class service(models.Model):
+    service_name = models.CharField(max_length = 100)
+    service_description = models.TextField(max_length =800)
+    service_price = models.IntegerField()
+   
+class booking(models.Model):
+    customer = models.ForeignKey(User, on_delete = models.CASCADE)
+    service_booked = models.ForeignKey(service , on_delete = models.CASCADE)
+    date_booked = models.DateTimeField(default = timezone.now)
+    start_time= models.DateTimeField()
 
     def __str__(self):
-        return f'Booking for {self.customer}'
+        return f'Booking for {self.customer} by {self.customer} '
+    
--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import service, booking
 
-# Create your views here.
+
+def service_request(request):
+    context = {
+        'services': service.objects.all()
+    }
+    return render(request, 'bookings/services.html', context)
+
+def booking_request(request):
+    context = {
+        'book': booking.objects.all()
+    }
+    return render(request, 'bookings/booking.html', context)
+
+
+
+
