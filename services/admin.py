d9e5c98fc40b136b029254f2d3f5dc28f17e0631
twesigetrejan <trejantwesige19@gmail.com>
2024-03-27 04:14:23 +0300

    rendered booking and services templates

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import service, booking
 
-# Register your models here.
+admin.site.register(service)
+admin.site.register(booking)
+
--- services/views.py
+++ services/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import service, booking
 
-# Create your views here.
+def service_request(request):
+    context = {
+        'services_provided': service.objects.all()
+    }
+    return(render, 'services/service.html', context)
+
+
+def booking_request(request):
+    context = {
+        'booking request' :booking.objects.all()
+    }
+    return(render, 'services/booking.html', context)
+
