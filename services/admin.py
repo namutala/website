773049f8d92a18c3056d081175bafedf4bceacf2
twesigetrejan <trejantwesige19@gmail.com>
2024-03-27 05:40:25 +0300

    allowed user to add many images for services

--- services/admin.py
+++ services/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import service, booking, serviceImage
+from .models import service, booking
 
 admin.site.register(service)
 admin.site.register(booking)
-admin.site.register(serviceImage)
 
 
--- services/models.py
+++ services/models.py
@@ -6,19 +6,15 @@ class service(models.Model):
     service_name = models.CharField(max_length = 100)
     service_description = models.CharField(default ='service description', max_length=600)
     service_prices = models.CharField(max_length = 100) 
+    image1 = models.ImageField(upload_to='service_images', blank=True)
+    image2 = models.ImageField(upload_to='service_images', blank=True)
+    image3 = models.ImageField(upload_to='service_images', blank=True)
+    image4 = models.ImageField(upload_to='service_images', blank=True)
+    image5 = models.ImageField(upload_to='service_images', blank=True)
+    image6 = models.ImageField(upload_to='service_images', blank=True)
     
     def __str__(self):
-        return {self.service_name}
-    
-class serviceImage(models.Model):
-    service = models.ForeignKey(service, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='service_images')
-    
-    def __str__(self):
-        return f'{self.service.service_name} Views'
-
-    def __str__(self):
-        return f"Image for {self.service.service_description}"
+        return self.service_name
     
 class booking(models.Model):
     customer = models.ForeignKey(User, on_delete = models.CASCADE)
