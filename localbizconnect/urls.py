b94903aa6e266876852f2dde186d8e5277d2ef17
Androbuzz <andricishami@gmail.com>
2024-04-09 09:31:19 +0300

    password change urls

--- localbizconnect/urls.py
+++ localbizconnect/urls.py
@@ -32,6 +32,8 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name="password_reset_confirm"),
     path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name="password_reset_done"),
     path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name="password_reset_complete"),
+    path('password-change/',auth_views.PasswordChangeView.as_view()),
+    path('password-change/done/',auth_views.PasswordChangeDoneView.as_view()),
     path('users/', include('users.urls')),
     path('business/', include('business.urls')),
     path('', include('storefront.urls')),
