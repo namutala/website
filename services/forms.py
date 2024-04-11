622f574a9c4798b623c705994b5d09eb8c480fc8
@KyomuhendoSumayah <lindafavour84@gmail.com>
2024-04-11 20:20:33 +0300

    updating

--- services/forms.py
+++ services/forms.py
@@ -4,4 +4,4 @@ from .models import booking
 class bookingform(forms.ModelForm):
     class Meta:
         Model = booking
-        fields = ['customer','service_booked', 'start_date', 'status', 'Durations']
+        fields = ['customer','service_booked', 'start_date', 'status']
