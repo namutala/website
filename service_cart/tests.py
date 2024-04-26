d81b9c0e0995e4764b52963a7239c668a2813866
Androbuzz <andricishami@gmail.com>
2024-04-26 22:45:36 +0300

    tests for service_cart

--- service_cart/tests.py
+++ service_cart/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+
+class ServiceCartTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='tester',
+            email='aik@yahoo.com',
+            password='secret'
+        )
+        self.client.force_login(username='tester', password='tester')
+
+    def test_service_cart(self):
+        response = self.client.get('/service_cart/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'service_cart/service_cart_summary.html')
+        response = self.client.get(reverse('service-cart-summary'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'service_cart/service_cart_summary.html')
